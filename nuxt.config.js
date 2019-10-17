4573920453c9b4bbaf8797d5878d87414547c903
planethouki <26595148+planethouki@users.noreply.github.com>
2019-10-18 08:43:03 +0900

    delete

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/CatapultTypes.js', '~/plugins/ComponentLoader.js'],
+  plugins: [],
   /*
    ** Nuxt.js dev-modules
    */
