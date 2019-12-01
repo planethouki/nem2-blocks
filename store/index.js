e8b43e01e95c00bb6519d7a6bd499d5fe006a8cd
planethouki <26595148+planethouki@users.noreply.github.com>
2019-12-01 16:33:23 +0900

    endpoint test net

--- store/index.js
+++ store/index.js
@@ -5,7 +5,7 @@ const host = (() => {
       return sessionStorageHost
     }
   }
-  return '54.65.82.119:3000'
+  return 'api-01.us-east-1.nemtech.network:3000'
 })()
 
 export const state = () => ({
