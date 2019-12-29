53867545f45e75d3c0c42c468f7ed724d5f6d977
planethouki <26595148+planethouki@users.noreply.github.com>
2019-12-29 18:17:49 +0900

    peers

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,7 @@
         <b-navbar-nav>
           <b-nav-item to="/blocks/0">Blocks</b-nav-item>
           <b-nav-item to="/statistics">Statistics</b-nav-item>
+          <b-nav-item to="/peers">Peers</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
---
+++ pages/peers.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="container-fluid">
+    <b-table
+      responsive
+      hover
+      small
+      primary-key="publicKey"
+      :items="peers"
+      :fields="fields"
+    >
+      <template v-slot:cell(publicKey)="data">
+        <a
+          :href="`${url}/account/${data.value}`"
+          target="_blank"
+          class="text-monospace"
+        >
+          {{ data.value }}
+        </a>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {},
+  data() {
+    return {
+      peers: [],
+      fields: [
+        { key: 'friendlyName', label: 'Name' },
+        { key: 'host' },
+        { key: 'port' },
+        { key: 'version', label: 'Ver' },
+        { key: 'roles' },
+        { key: 'networkIdentifier', label: 'NW' },
+        { key: 'publicKey', label: 'Public Key' }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters(['url'])
+  },
+  mounted() {
+    this.get()
+    this.$store.subscribe((mutation, state) => {
+      if (
+        mutation.type === 'newBlock' &&
+        state.newBlock.block.height !== undefined
+      ) {
+        this.$nextTick(() => {
+          this.get()
+        })
+      }
+    })
+  },
+  destroyed() {},
+  methods: {
+    get() {
+      this.$axios.$get(`${this.url}/node/peers`).then((res) => {
+        this.peers = res
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #35495e;
+  text-decoration: underline;
+}
+a:hover {
+  text-decoration: none;
+}
+.card {
+  margin-top: 1rem;
+}
+</style>
