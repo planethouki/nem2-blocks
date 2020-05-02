eae856cbd87d1a9d972410bd5d7a8e461369e77d
planethouki <26595148+planethouki@users.noreply.github.com>
2020-05-02 17:26:10 +0900

    endpoint

--- store/index.js
+++ store/index.js
@@ -5,7 +5,7 @@ const host = (() => {
       return sessionStorageHost
     }
   }
-  return 'pentesting1-api.48gh23s.xyz:3001'
+  return 'pentesting2-api.48gh23s.xyz:3001'
 })()
 
 export const state = () => ({
